1f698746c6744d9eef6613d4c39faddb54b54155
maheedhar-reddy <maheedhar343@gmail.com>
2024-01-21 14:25:18 +0530

    finalproject

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+function Hello(){
+    return(
+      <div>
+        <h1>Coding Club</h1>
+        <p>Charan</p>
+        <h2>Wellcome to coding club</h2>
+      </div>
+    )
+  }
+  
+  export default Hello;
